a20fac79e081a882aa818ce5a22c974c4ebef3e8
natalie <natallie79@gmail.com>
2023-05-25 15:30:17 -0700

    25.05

--- cypress/e2e/profile.cy.js
+++ cypress/e2e/profile.cy.js
@@ -1,10 +1,11 @@
 import {ProfilePage} from '../pages'
+import  * as color from '../fixtures/color.json'
 
 describe('Profile', () => {
   beforeEach(() => {
     cy.intercept(
       'GET',
-      `${Cypress.env('apiBaseUrl')}/course/coursesProgress/${Cypress.env('userId')}`,
+      '/course/coursesProgress/*',
       {
         statusCode: 200,
         body: {
@@ -14,7 +15,7 @@ describe('Profile', () => {
           payload: [
             {
               _id: '6465b638cc28f14deeb4db0d',
-              completedLessons: 114,
+              completedLessons: 0,
               totalLessons: 114,
               course: {
                 _id: '635c05ba659f6d77abf1f4e7',
@@ -36,7 +37,69 @@ describe('Profile', () => {
     cy.location('pathname').should('eq', '/')
   })
 
-  it('Course progress', () => {
+  it.only('Course progress', () => {
+    cy.get('.ant-progress-text').should('have.text', '0%')
+
+    cy.intercept(
+        'GET',
+        '/course/coursesProgress/*',
+        {
+          statusCode: 200,
+          body: {
+            message: 'Get courses progress',
+            success: true,
+            fail: false,
+            payload: [
+              {
+                _id: '6465b638cc28f14deeb4db0d',
+                completedLessons: 114,
+                totalLessons: 114,
+                course: {
+                  _id: '635c05ba659f6d77abf1f4e7',
+                  name: 'Python syntax',
+                },
+              },
+            ],
+          },
+        }
+    )
+    cy.reload()
+    cy.get('.anticon-check-circle').should('be.visible')
+    cy.get('.ant-progress-bg')
+        .should('be.visible')
+        .should('have.css', 'background-color', color.green)
+
+
+
+
+    cy.intercept(
+        'GET',
+        '/course/coursesProgress/*',
+        {
+          statusCode: 200,
+          body: {
+            message: 'Get courses progress',
+            success: true,
+            fail: false,
+            payload: [
+              {
+                _id: '6465b638cc28f14deeb4db0d',
+                completedLessons: 57,
+                totalLessons: 114,
+                course: {
+                  _id: '635c05ba659f6d77abf1f4e7',
+                  name: 'Python syntax',
+                },
+              },
+            ],
+          },
+        }
+    )
+    cy.reload()
+    cy.get('.ant-progress-text').should('have.text', '50%')
+    cy.get('.ant-progress-bg')
+        .should('be.visible')
+        .should('have.css', 'background-color', color.blue)
 
   })
 })
