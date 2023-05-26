a8d3b2fc9f1da9dbcf8c14a332c670e065ed8868
natalie <natallie79@gmail.com>
2023-05-25 20:02:26 -0700

    25.05

--- cypress/e2e/profile.cy.js
+++ cypress/e2e/profile.cy.js
@@ -1,33 +1,30 @@
 import {ProfilePage} from '../pages'
-import  * as color from '../fixtures/color.json'
+import * as color from '../fixtures/color.json'
 
 describe('Profile', () => {
   beforeEach(() => {
-    cy.intercept(
-      'GET',
-      '/course/coursesProgress/*',
-      {
-        statusCode: 200,
-        body: {
-          message: 'Get courses progress',
-          success: true,
-          fail: false,
-          payload: [
-            {
-              _id: '6465b638cc28f14deeb4db0d',
-              completedLessons: 0,
-              totalLessons: 114,
-              course: {
-                _id: '635c05ba659f6d77abf1f4e7',
-                name: 'Python syntax',
-              },
+    cy.intercept('GET', '/course/coursesProgress/*', {
+      statusCode: 200,
+      body: {
+        message: 'Get courses progress',
+        success: true,
+        fail: false,
+        payload: [
+          {
+            _id: '6465b638cc28f14deeb4db0d',
+            completedLessons: 0,
+            totalLessons: 114,
+            course: {
+              _id: '635c05ba659f6d77abf1f4e7',
+              name: 'Python syntax',
             },
-          ],
-        },
-      }
-    )
+          },
+        ],
+      },
+    })
     cy.loginByToken()
     ProfilePage.open()
+    cy.intercept({resourceType: /xhr|fetch/}, {log: false})
   })
 
   it('Sign out', () => {
@@ -40,66 +37,54 @@ describe('Profile', () => {
   it.only('Course progress', () => {
     cy.get('.ant-progress-text').should('have.text', '0%')
 
-    cy.intercept(
-        'GET',
-        '/course/coursesProgress/*',
-        {
-          statusCode: 200,
-          body: {
-            message: 'Get courses progress',
-            success: true,
-            fail: false,
-            payload: [
-              {
-                _id: '6465b638cc28f14deeb4db0d',
-                completedLessons: 114,
-                totalLessons: 114,
-                course: {
-                  _id: '635c05ba659f6d77abf1f4e7',
-                  name: 'Python syntax',
-                },
-              },
-            ],
+    cy.intercept('GET', '/course/coursesProgress/*', {
+      statusCode: 200,
+      body: {
+        message: 'Get courses progress',
+        success: true,
+        fail: false,
+        payload: [
+          {
+            _id: '6465b638cc28f14deeb4db0d',
+            completedLessons: 114,
+            totalLessons: 114,
+            course: {
+              _id: '635c05ba659f6d77abf1f4e7',
+              name: 'Python syntax',
+            },
           },
-        }
-    )
+        ],
+      },
+    })
     cy.reload()
     cy.get('.anticon-check-circle').should('be.visible')
     cy.get('.ant-progress-bg')
-        .should('be.visible')
-        .should('have.css', 'background-color', color.green)
+      .should('be.visible')
+      .should('have.css', 'background-color', color.green)
 
-
-
-
-    cy.intercept(
-        'GET',
-        '/course/coursesProgress/*',
-        {
-          statusCode: 200,
-          body: {
-            message: 'Get courses progress',
-            success: true,
-            fail: false,
-            payload: [
-              {
-                _id: '6465b638cc28f14deeb4db0d',
-                completedLessons: 57,
-                totalLessons: 114,
-                course: {
-                  _id: '635c05ba659f6d77abf1f4e7',
-                  name: 'Python syntax',
-                },
-              },
-            ],
+    cy.intercept('GET', '/course/coursesProgress/*', {
+      statusCode: 200,
+      body: {
+        message: 'Get courses progress',
+        success: true,
+        fail: false,
+        payload: [
+          {
+            _id: '6465b638cc28f14deeb4db0d',
+            completedLessons: 57,
+            totalLessons: 114,
+            course: {
+              _id: '635c05ba659f6d77abf1f4e7',
+              name: 'Python syntax',
+            },
           },
-        }
-    )
+        ],
+      },
+    })
     cy.reload()
     cy.get('.ant-progress-text').should('have.text', '50%')
     cy.get('.ant-progress-bg')
-        .should('be.visible')
-        .should('have.css', 'background-color', color.blue)
-
+      .should('be.visible')
+      .should('have.css', 'background-color', color.blue)
   })
 })
